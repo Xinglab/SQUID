77702c15968c0101c2ebf99b5567f7a4e4b393b7
sfli001 <sfli001@gmail.com>
2016-04-05 09:54:08 -0700

    Added files via upload

---
+++ Clean_Intron_strand.py
@@ -0,0 +1,139 @@
+#!/u/local/apps/python/2.7.3/bin/python
+import copy, getopt,re,os,sys,logging,time,datetime;
+
+options, args = getopt.getopt(sys.argv[1:],'', ['gtf=','path=','length=','anchor=','strand='])
+gtf = ''
+path=''
+strand = 'unstrand'
+length =100
+anchor =8
+for opt, arg in options:
+        if opt in('--gtf'):
+                gtf = arg
+        elif opt in('--path'):
+                path = arg
+	elif opt in('--length'):
+                length = int(arg)
+	elif opt in('--anchor'):
+                anchor = int(arg)
+	elif opt in('--strand'):
+                strand = arg
+if (not gtf or not path):
+        print "not enough parameters"
+        print "Program : ", sys.argv[0]
+        print "a python program to get the clean intron  from given intron gtf file"
+        print "usage :", sys.argv[0], " --gtf: gtf file"
+        print "usage :", sys.argv[0], " --path: the directory to the input and output gtf file"
+	print "usage :", sys.argv[0], " --length: the read length"
+	print "usage :", sys.argv[0], " --anchor: the anchor length"
+        print "usage :", sys.argv[0], " --strand: whether the library contains strand info"
+	print datetime.datetime.now()
+        print "Author  : Shaofang Li"
+        print "Contact : sfli001@gmail.com"
+        sys.exit()
+
+fr = open ("%s/Intron_%s" % (path,gtf))
+fw = open(("%s/Intron_clean_%s" %(path,gtf)),"w")
+
+
+
+pos = dict()
+bin = 1000
+intron = dict()
+intron5= dict()
+intron3 = dict()
+for info1 in fr:
+	a1 = info1.strip().split("\t")
+	key = "%s_%s_%s" % (a1[0],a1[3],a1[4])
+	intron[key]=["true","true","true","true",a1[0],int(a1[3]),int(a1[4])]
+	intron5.setdefault((a1[0],int(a1[3])),[]).append(int(a1[4]))	
+        intron3.setdefault((a1[0],int(a1[4])),[]).append(int(a1[3]))
+	index_s =  int(a1[3])/ bin
+        index_e =  int(a1[4])/ bin
+	if(strand=="unstrand"):
+		for i in  range(index_s , index_e+1):
+                        pos.setdefault((a1[0],i),[]).append(key)
+	else:
+        	for i in  range(index_s , index_e+1):
+                	pos.setdefault((a1[0],a1[6],i),[]).append(key)
+		
+fr.close()
+for kk in intron5:
+	intron5[kk] = list(set(intron5[kk]))
+for kk in intron3:
+        intron3[kk] = list(set(intron3[kk]))
+
+for kk in intron:
+	for ee in intron5[intron[kk][4],intron[kk][5]]:
+		if(ee!=intron[kk][6]):
+			intron[kk][3]="false"
+	for ss in intron3[intron[kk][4],intron[kk][6]]:
+		if(ss!=intron[kk][5]):
+			intron[kk][3]="false"
+
+fr2 = open ("%s/Exon_%s" % (path,gtf))
+for info2 in fr2:
+        a2 = info2.strip().split("\t")
+	gene_id = re.sub('.*gene_id "|\".*','',a2[8])
+        index_s =  int(a2[3]) / bin
+        index_e =  int(a2[4]) / bin
+	if(strand=="unstrand"):
+                for i in  range(index_s , index_e+1):
+                        if (a2[0], i) in pos:
+                                        for j in pos[a2[0], i]: 
+                                                if(int(a2[3]) <= intron[j][5]):
+                                                        if(int(a2[4]) >= intron[j][5]):
+                                                                intron[j][0]="false"
+                                                                intron[j][1]="false"
+                                                if( (int(a2[3]) > intron[j][5]) and  (int(a2[3]) <= intron[j][6])):
+                                                        intron[j][0]="false"
+                                                if( (int(a2[3]) > intron[j][5]) and (int(a2[3]) <= min(intron[j][6],intron[j][5]+ length -anchor +1))):
+                                                        intron[j][1]="false"
+                                                        if(intron[j][0]=="true"):
+                                                                print info2, intron[j]
+                                                if(int(a2[3]) <= max(intron[j][5], intron[j][6]-length+ anchor+1)):
+                                                        if(int(a2[4]) >= max(intron[j][5], intron[j][6]-length+ anchor+1)):
+                                                                intron[j][2]="false"
+                                                if( (int(a2[3]) > max(intron[j][5],intron[j][6]-length+anchor +1)) and  (int(a2[3]) <= intron[j][6])):                                                        intron[j][2]="false"
+                                                                        
+                                                if(intron[j][0] =="true" and intron[j][2]=="false"):
+                                                        print info2, intron[j]
+	else:	
+		for i in  range(index_s , index_e+1):
+			if (a2[0], a2[6], i) in pos:
+					for j in pos[a2[0],a2[6], i]:
+						if(int(a2[3]) <= intron[j][5]):
+							if(int(a2[4]) >= intron[j][5]):
+								intron[j][0]="false"
+								intron[j][1]="false"
+						if( (int(a2[3]) > intron[j][5]) and  (int(a2[3]) <= intron[j][6])):
+							intron[j][0]="false"
+						if( (int(a2[3]) > intron[j][5]) and (int(a2[3]) <= min(intron[j][6],intron[j][5]+ length -anchor +1))):
+							intron[j][1]="false"
+							if(intron[j][0]=="true"):
+								print info2, intron[j]
+						if(int(a2[3]) <= max(intron[j][5], intron[j][6]-length+ anchor+1)):
+							if(int(a2[4]) >= max(intron[j][5], intron[j][6]-length+ anchor+1)):
+								intron[j][2]="false"
+						if( (int(a2[3]) > max(intron[j][5],intron[j][6]-length+anchor +1)) and  (int(a2[3]) <= intron[j][6])):
+							intron[j][2]="false"	
+									
+						if(intron[j][0] =="true" and intron[j][2]=="false"):
+							print info2, intron[j]		
+						
+					
+
+fr2.close()
+
+fr = open ("%s/Intron_%s" % (path,gtf))
+for info1 in fr:
+	a1 = info1.strip().split("\t")
+	key = "%s_%s_%s" % (a1[0],a1[3],a1[4])
+	if(a1[6]=="+"):
+		fw.write('%s clean "%s"; clean_5end "%s"; clean_3end "%s"; clean_simple "%s";\n' %(info1.strip(),intron[key][0],intron[key][1],intron[key][2], intron[key][3]))
+	if(a1[6]=="-"):
+		fw.write('%s clean "%s"; clean_5end "%s"; clean_3end "%s"; clean_simple "%s";\n' %(info1.strip(),intron[key][0],intron[key][2],intron[key][1],intron[key][3]))
+		
+fr.close()
+fw.close()
+
---
+++ Intron_countall_strandSam.py
@@ -0,0 +1,424 @@
+#!/u/home/s/shaofang/project-yxing/soft/pp/bin/python
+import getopt,copy,re,os,sys,logging,time,datetime;
+options, args = getopt.getopt(sys.argv[1:], 'o:',['gtf=','anchor=','lib=','read=','length=','sam=','output=',])
+gtf='';
+read='';
+sam='';
+lib='';
+length =0
+anchor =0
+output='';
+for opt, arg in options:
+	if opt in ('-o','--output'):
+		output = arg
+	elif opt in ('--gtf'):
+		gtf = arg
+	elif opt in ('--sam'):
+                sam = arg
+	elif opt in ('--lib'):
+                lib = arg
+	elif opt in ('--read'):
+                read = arg
+	elif opt in ('--length'):
+                length= int(arg)
+	elif opt in ('--anchor'):
+                anchor= int(arg)
+if (not gtf or not read or not sam or not output or not length or not anchor):
+	print "not enough parameters"
+	print "Program : ", sys.argv[0]
+	print "a python program to count the reads for Intron Retention events for varities of junction from a series of sam file"
+	print "usage :", sys.argv[0], " --gtf: the gtf file of the intron annotation"
+	print "usage :", sys.argv[0], " --length:the length of reads"
+	print "usage :", sys.argv[0], " --anchor:the anchor length of the read"
+	print "usage :", sys.argv[0], " --sam: the sam file,multiple sam file seperated through ','"
+	print "usage :", sys.argv[0], " --lib: choice (unstrand, first, second) the library type, indication the strand ','"
+	print "usage :", sys.argv[0], " --read: (P,S) the library type, indication the strand ','"
+	print "usage :", sys.argv[0], ' --output: intron_id, gene_id,strand,chr,start,end,5SS inclusion counts,5SS skipping counts,3SS includion counts,3SS skipping counts,skipping counts,intron counts'
+	print datetime.datetime.now()
+	print "Author  : Shaofang Li"
+	print "Contact : sfli001@gmail.com"
+	sys.exit()
+if(lib=="unstrand"):
+	print "This is the library without strand information"
+
+	fr1 = open(gtf)
+	count = dict()
+	#the dict to store the intron count,first is the inclusion at left side, second is the skipped count at smaller side, third inclusion at right side, fourth skipping at right side, fifith skipping counts, sixth intron counts"
+	ppL = dict()
+	#the dict to store left edge  position of intron
+	ppR = dict()
+	#the dict to store right  edge  position of intron
+	pp = dict()
+	#the dict to store the two edge position of intron
+	pos = dict()
+	# the dictionary to store the introns in a certain windows
+	bin =1000
+	# set the bin size
+	samfile = sam.split(",")
+	num = len(samfile)
+	for info1 in fr1:
+		a1 = info1.strip().split("\t")
+		key = "%s_%s_%s" % (a1[0],a1[3],a1[4])
+		gene_id = re.sub('.*gene_id "|\".*','',a1[8])
+		if(count.has_key(key)):
+			gg = count[key][6*num].split(",")
+			gg_l = "true"
+			for g_id in gg:
+				if(gene_id == g_id):
+					gg_l = "falase"
+			if(gg_l =="true"):
+				count[key][6*num]+=","
+				count[key][6*num]+=gene_id
+				count[key][6*num+1]+=","
+				count[key][6*num+1]+=a1[6]
+
+		else:
+			count[key]= [0]*num*6
+			count[key].append(gene_id)
+			count[key].append(a1[6])
+			count[key].append("true")
+			ppL.setdefault((a1[0],int(a1[3])),[]).append(key)
+                	ppR.setdefault((a1[0],int(a1[4])),[]).append(key)
+			pp[a1[0],int(a1[3]),int(a1[4])] = key
+			index_s =  int(a1[3]) / bin
+               		index_e =  int(a1[4]) / bin
+                	for i in  range(index_s , index_e+1):
+                        	pos.setdefault((a1[0],i),[]).append(key)
+	fr1.close()
+	samfile = sam.split(",")
+	nn = 0
+	for file in samfile:
+		fr2 = open(file)
+		for info2 in fr2:
+			if (info2[0]== "@"):
+				continue
+			a2 = info2.strip().split("\t")
+			if(read =="P"):
+				if((int(a2[1])/2 )%2 ==0):
+					continue
+                                if(re.search("NH:i:1[0-9]$",info2)):
+                                        continue
+				if(re.search("NH:i:[^1]$",info2)):
+                                        continue
+                                if(re.search("D",a2[5])):
+                                        continue
+                                if(re.search("I",a2[5])):
+                                        continue
+                        if(read =="S"):
+				if(re.search("NH:i:1[0-9]$",info2)):
+                                        continue
+                                if(re.search("NH:i:[^1]$",info2)):
+                                        continue
+                                if(re.search("D",a2[5])):
+                                        continue
+                                if(re.search("I",a2[5])):
+                                        continue
+                        aa1 = a2[5].split("M")
+                        aa2 = a2[5].split("N")
+                        l = len(aa2)
+			if(len(aa1)== 2):
+				for p in range(int(a2[3]) + anchor, int(a2[3]) + int(aa1[0]) - anchor +1):
+					if(a2[2],p) in ppL:
+						for id in ppL[a2[2],p]:
+							#inclusion count at left side
+							count[id][nn*6] +=1
+							continue
+				for p in range(int(a2[3]) + anchor-1, int(a2[3]) + int(aa1[0]) - anchor):
+					if(a2[2],p) in ppR:
+                                                for id in ppR[a2[2],p]:
+                                                        #inclusion count at right side
+                                                        count[id][nn*6+2] +=1 
+                                                        continue	
+				index= int(a2[3])/bin
+                                if (a2[2], index) in pos:
+                                        for key in pos[a2[2],index]:
+						in_p = key.split("_")
+                                                if ((int(in_p[1]) <= int(a2[3]) )& (int(in_p[2])>= (int(a2[3]) + length -1))):
+                                                        count[key][nn*6+5]+=1
+			start = int(a2[3])
+			for i in range(0,l-1):
+				if(re.search("\D",aa2[i].split("M")[0])):
+					continue
+				if(re.search("\D",aa2[i].split("M")[1])):
+					continue
+				if(re.search("\D",aa2[i+1].split("M")[0])):
+					continue
+				n1=int(aa2[i].split("M")[0])
+				n2=int(aa2[i].split("M")[1])
+				n3=int(aa2[i+1].split("M")[0])
+				if (n1 >= anchor and n3 >= anchor):
+					if (a2[2], start+n1) in ppL:
+						for id in ppL[a2[2],start +n1]:
+							#skipped count at left side
+							count[id][nn*6+1] +=1
+					if (a2[2], start+n1+n2-1) in ppR:
+                                                for id in ppR[a2[2],start +n1+n2 -1]:
+                                                        #skipped count at right side
+                                                        count[id][nn*6+3] +=1
+					if (a2[2],start+n1, start+n1+n2-1) in pp:
+						#skipping count at both side
+        					count[pp[a2[2],start+n1,start +n1+n2 -1]][nn*6+4] +=1	
+
+				if( n1 -anchor+1 >  anchor):
+					ss = start
+					for p in range( ss + anchor, ss + n1 -anchor+1 ):
+						if(a2[2],p) in ppL:
+							for id in ppL[a2[2],p]:
+								#included count at left side
+								count[id][nn*6] +=1
+					for p in range( ss + anchor-1, ss + n1 -anchor ):
+                                                if(a2[2],p) in ppR:
+                                                        for id in ppR[a2[2],p]:
+								#included count at right side
+                                                                count[id][nn*6+2] +=1
+				if( n3 -anchor+1 >  anchor):
+					ss = start + n1 + n2
+					for p in range( ss + anchor, ss + n3 -anchor+1 ):
+						if(a2[2],p) in ppL:
+							for id in ppL[a2[2],p]:
+								#included count at left side
+								count[id][nn*6] +=1
+					for p in range( ss + anchor-1, ss + n3 -anchor):
+						if(a2[2],p) in ppR:
+                                                        for id in ppR[a2[2],p]:
+                                                                #included count at left side
+                                                                count[id][nn*6+2] +=1
+
+				start += (n1+n2)
+
+		nn += 1
+		fr2.close()
+	fr1 = open(gtf)
+	fw = open(output,"w")
+	for info1 in fr1:
+		a1 = info1.strip().split("\t")
+		key = "%s_%s_%s" % (a1[0],a1[3],a1[4])
+		if(count[key][num*6+2]=="true"):
+			if(re.search('\+',count[key][num*6+1])):
+				fw.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (key, count[key][num*6],count[key][num*6+1],a1[0],a1[3],a1[4],"\t".join(str(x) for x in count[key][0:num*6])))
+			else:
+				fw.write("%s\t%s\t%s\t%s\t%s\t%s" % (key, count[key][num*6],count[key][num*6+1],a1[0],a1[3],a1[4]))
+				for i in range(0, num):
+					fw.write("\t%s\t%s\t%s\t%s\t%s\t%s"%(count[key][i*6+2],count[key][i*6+3],count[key][i*6],count[key][i*6+1],count[key][i*6+4],count[key][i*6+5]))
+				fw.write("\n") 
+			count[key][num*6+2]="false"
+	fw.close()
+	exit()
+
+print "this is library with strand info"
+fr1 = open(gtf)
+count = dict()
+#the dict to store the intron count,first is the inclusion at left side, second is the skipped count at smalller side, third inclusion at right side, fourth skipping at right side
+ppL = dict()
+#the dict to store left edge  position of intron
+ppR = dict()
+#the dict to store right  edge  position of intron
+pp = dict()
+#the dict to store the two edge position of intron
+pos = dict()
+# the dictionary to store the introns in a certain windows
+bin =1000
+# set the bin size
+
+samfile = sam.split(",")
+num = len(samfile)
+for info1 in fr1:
+	a1 = info1.strip().split("\t")
+	key = "%s_%s_%s" % (a1[0],a1[3],a1[4])
+        gene_id = re.sub('.*gene_id "|\".*','',a1[8])
+	if(count.has_key(key)):
+		gg = count[key][6*num].split(",")
+		gg_l = "true"
+		for g_id in gg:
+			if(gene_id == g_id):
+				gg_l = "falase"
+		if(gg_l =="true"):
+			count[key][6*num]+=","
+			count[key][6*num]+=gene_id
+			count[key][6*num+1]+=","
+			count[key][6*num+1]+=a1[6]
+
+	else:
+		count[key]= [0]*num*6
+		count[key].append(gene_id)
+		count[key].append(a1[6])
+		count[key].append("true")
+		ppL.setdefault((a1[0],int(a1[3])),[]).append(key)
+		ppR.setdefault((a1[0],int(a1[4])),[]).append(key)
+		pp[a1[0],int(a1[3]),int(a1[4])] = key
+		index_s =  int(a1[3]) / bin
+		index_e =  int(a1[4]) / bin
+                for i in  range(index_s , index_e+1):
+                	pos.setdefault((a1[0],i),[]).append(key)
+
+fr1.close()
+samfile = sam.split(",")
+nn = 0
+for file in samfile:
+        fr2 = open(file)
+        for info2 in fr2:
+                if (info2[0]== "@"):
+                        continue
+		strand=''
+                a2 = info2.strip().split("\t")
+		if(read =="P"):
+			if((int(a2[1])/2 )%2 ==0):
+				continue
+			if(re.search("NH:i:1[0-9]$",info2)):
+				continue
+			if(re.search("NH:i:[^1]$",info2)):
+				continue
+			if(re.search("D",a2[5])):
+				continue
+			if(re.search("I",a2[5])):
+				continue
+		if(read =="S"):
+			if(re.search("NH:i:1[0-9]$",info2)):
+				continue
+			if(re.search("NH:i:[^1]$",info2)):
+				continue
+			if(re.search("D",a2[5])):
+				continue
+			if(re.search("I",a2[5])):
+				continue
+
+                flag = str(length) +'M' 
+		ss = int(a2[1]) /16 % 2
+			
+		if ((read =="S") & (lib=="first")):
+			if (ss==0):
+				strand="-"
+			else:
+				strand="+"
+		if ((read =="S") & (lib=="second")):
+                        if (ss==0):
+                                strand="+"
+                        else:
+                                strand="-"
+		
+		if ((read =="P") & (lib =="first")):
+			f = int(a2[1]) /64 % 2
+			s = int(a2[1]) /128 % 2
+			if ((ss == 0) & (f ==1)):
+				strand = "-"
+			if ((ss == 0) & (s ==1)):
+                                strand = "+"
+			if ((ss == 1) & (f ==1)):
+                                strand = "+"
+                        if ((ss == 1) & (s ==1)):
+                                strand = "-"
+		
+		if ((read =="P") & (lib =="second")):
+                        f = int(a2[1]) /64 % 2
+                        s = int(a2[1]) /128 % 2
+                        if ((ss == 0) & (f ==1)):
+                                strand = "+"
+                        if ((ss == 0) & (s ==1)):
+                                strand = "-"
+                        if ((ss == 1) & (f ==1)):
+                                strand = "-"
+                        if ((ss == 1) &( s ==1)):
+                                strand = "+"
+		if (strand==''):
+			print a2
+       
+		aa1 = a2[5].split("M")
+		aa2 = a2[5].split("N")
+		l = len(aa2)
+		if(len(aa1)== 2):
+			for p in range(int(a2[3]) + anchor, int(a2[3]) + int(aa1[0]) - anchor +1):
+				if(a2[2],p) in ppL:
+					for id in ppL[a2[2],p]:
+						if(re.search(("\%s" %strand),count[id][num*6+1])):
+							#inclusion count at left side
+							count[id][nn*6] +=1
+							continue
+			for p in range(int(a2[3]) + anchor-1, int(a2[3]) + int(aa1[0]) - anchor):
+				if(a2[2],p) in ppR:
+					for id in ppR[a2[2],p]:
+						if(re.search(("\%s" %strand),count[id][num*6+1])):
+							#inclusion count at right side
+							count[id][nn*6+2] +=1 
+							continue        
+			index= int(a2[3])/bin
+			if (a2[2], index) in pos:
+				for key in pos[a2[2],index]:
+					in_p = key.split("_")
+					if ((int(in_p[1]) <= int(a2[3]) )& (int(in_p[2])>= (int(a2[3]) + length -1))):
+						if (re.search(("\%s" %strand),count[key][num*6+1])):
+							count[key][nn*6+5]+=1
+		start = int(a2[3])
+		for i in range(0,l-1):
+			if(re.search("\D",aa2[i].split("M")[0])):
+				continue
+			if(re.search("\D",aa2[i].split("M")[1])):
+				continue
+			if(re.search("\D",aa2[i+1].split("M")[0])):
+				continue
+			n1=int(aa2[i].split("M")[0])
+			n2=int(aa2[i].split("M")[1])
+			n3=int(aa2[i+1].split("M")[0])
+			if (n1 >= anchor and n3 >= anchor):
+				if (a2[2], start+n1) in ppL:
+					for id in ppL[a2[2],start +n1]:
+						if(re.search(("\%s" %strand),count[id][num*6+1])):
+							#skipped count at left side
+							count[id][nn*6+1] +=1
+				if (a2[2], start+n1+n2-1) in ppR:
+					for id in ppR[a2[2],start +n1+n2 -1]:
+						if(re.search(("\%s" %strand),count[id][num*6+1])):
+							#skipped count at right side
+							count[id][nn*6+3] +=1
+				if (a2[2],start+n1, start+n1+n2-1) in pp:
+					if(re.search(("\%s" %strand),count[pp[a2[2],start+n1,start +n1+n2 -1]][num*6+1])):
+						#skipping count at both side
+						count[pp[a2[2],start+n1,start +n1+n2 -1]][nn*6+4] +=1   
+		    	if( n1 -anchor+1 >  anchor):
+				ss = start
+				for p in range( ss + anchor, ss + n1 -anchor+1 ):
+					if(a2[2],p) in ppL:
+						for id in ppL[a2[2],p]:
+							if(re.search(("\%s" %strand),count[id][num*6+1])):
+								#included count at left side
+								count[id][nn*6] +=1
+				for p in range( ss + anchor-1, ss + n1 -anchor ):
+					if(a2[2],p) in ppR:
+						for id in ppR[a2[2],p]:
+							if(re.search(("\%s" %strand),count[id][num*6+1])):
+								#included count at right side
+								count[id][nn*6+2] +=1
+			if( n3 -anchor+1 >  anchor):
+				ss = start + n1 + n2
+				for p in range( ss + anchor, ss + n3 -anchor+1 ):
+					if(a2[2],p) in ppL:
+						for id in ppL[a2[2],p]:
+							if(re.search(("\%s" %strand),count[id][num*6+1])):
+								#included count at left side
+								count[id][nn*6] +=1
+				for p in range( ss + anchor-1, ss + n3 -anchor):
+					if(a2[2],p) in ppR:
+						for id in ppR[a2[2],p]:
+							if(re.search(("\%s" %strand),count[id][num*6+1])):
+								#included count at left side
+								count[id][nn*6+2] +=1
+
+			start += (n1+n2)
+	nn += 1
+	fr2.close()
+
+fr1 = open(gtf)
+fw = open(output,"w")
+for info1 in fr1:
+	a1 = info1.strip().split("\t")
+	key = "%s_%s_%s" % (a1[0],a1[3],a1[4])
+	if(count[key][num*6+2]=="true"):
+		if(re.search('\+',count[key][num*6+1])):
+			fw.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (key, count[key][num*6],count[key][num*6+1],a1[0],a1[3],a1[4],"\t".join(str(x) for x in count[key][0:num*6])))
+		else:
+			fw.write("%s\t%s\t%s\t%s\t%s\t%s" % (key, count[key][num*6],count[key][num*6+1],a1[0],a1[3],a1[4]))
+			for i in range(0, num):
+				fw.write("\t%s\t%s\t%s\t%s\t%s\t%s"%(count[key][i*6+2],count[key][i*6+3],count[key][i*6],count[key][i*6+1],count[key][i*6+4],count[key][i*6+5]))
+			fw.write("\n") 
+		count[key][num*6+2]="false"
+fw.close()
+
